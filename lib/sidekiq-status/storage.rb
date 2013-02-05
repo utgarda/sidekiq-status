fcf9372adb68b46295ea6c2c57cf238bf6686e20
Andrew Korzhuev <wolfon@gmail.com>
2013-02-06 00:15:18 +0400

    Fix bug if we're trying to write array as param

--- lib/sidekiq-status/storage.rb
+++ lib/sidekiq-status/storage.rb
@@ -11,7 +11,7 @@ module Sidekiq::Status::Storage
   def store_for_id(id, status_updates, expiration = nil)
     Sidekiq.redis do |conn|
       conn.multi do
-        conn.hmset  id, 'update_time', Time.now.to_i, *(status_updates.to_a.flatten)
+        conn.hmset  id, 'update_time', Time.now.to_i, *(status_updates.to_a.flatten(1))
         conn.expire id, (expiration || Sidekiq::Status::DEFAULT_EXPIRY)
         conn.publish "status_updates", id
       end[0]
