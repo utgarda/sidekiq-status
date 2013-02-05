76aa329ba8d25a3737daa2c4d5b81f7ea94efd95
Andrew Korzhuev <wolfon@gmail.com>
2013-02-05 22:48:56 +0400

    Correct types for returning values of num,total,etc. status method instead of get to retrieve job status, now get returns any field to you in string format. Updated readme

--- lib/sidekiq-status.rb
+++ lib/sidekiq-status.rb
@@ -4,45 +4,53 @@ require 'sidekiq-status/worker'
 require 'sidekiq-status/client_middleware'
 require 'sidekiq-status/server_middleware'
 
-module Sidekiq
-  module Status
-    extend Storage
-    DEFAULT_EXPIRY = 60 * 30
-    STATUS = %w(queued working complete stopped failed).map(&:to_sym).freeze
+module Sidekiq::Status
+  extend Storage
+  DEFAULT_EXPIRY = 60 * 30
+  STATUS = %w(queued working complete stopped failed).map(&:to_sym).freeze
 
-    [:status, :num, :total, :message].each do |name|
-      class_eval(<<-END, __FILE__, __LINE__)
-        def self.#{name}(job_id)
-          read_field_for_id job_id, :#{name}
-        end
-      END
-    end
-
-    STATUS.each do |name|
-      class_eval(<<-END, __FILE__, __LINE__)
-        def self.#{name}?(job_id)
-          get(job_id).to_sym == :#{name}
-        end
-      END
-    end
-
-    # TODO make #get synonym to #read_field_for_id and use #status instead of #get
+  class << self
     # Job status by id
     # @param [String] id job id returned by async_perform
     # @return [String] job status, possible values are in STATUS
-    def self.get(id)
-      read_field_for_id(id, :status)
-    end
-
-    def self.pct_complete(id)
-      (num / total) * 100
+    def get(job_id, field)
+      read_field_for_id(job_id, field)
     end
 
     # Get all status fields for a job
     # @params [String] id job id returned by async_perform
     # @return [Hash] hash of all fields stored for the job
-    def self.get_all(id)
+    def get_all(id)
       read_hash_for_id(id)
+    end
+
+    def status(job_id)
+      get(job_id, :status).to_sym
+    end
+
+    STATUS.each do |name|
+      class_eval(<<-END, __FILE__, __LINE__)
+        def self.#{name}?(job_id)
+          status(job_id) == :#{name}
+        end
+      END
+    end
+
+    # Methods for retrieving job completion
+    def num(job_id)
+      get(job_id, :num).to_i
+    end
+
+    def total(job_id)
+      get(job_id, :total).to_i
+    end
+
+    def pct_complete(job_id)
+      (num(job_id) / total(job_id)) * 100
+    end
+
+    def message(job_id)
+      get(job_id, :message)
     end
   end
 end
