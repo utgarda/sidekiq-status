6c6bc4a3373cb9b75f8d5764890706d15d14d189
Pramod <pramod@joshsoftware.com>
2016-06-21 22:50:12 +0530

    Fixed different format of arguments - fixes #79
    - kept backword compatibility for arguments displaying with Sidekiq ~> 2.7
    - fixed uninitialized constant for Sidekiq::Job

--- lib/sidekiq-status/client_middleware.rb
+++ lib/sidekiq-status/client_middleware.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/api'
 module Sidekiq::Status
 # Should be in the client middleware chain
   class ClientMiddleware
@@ -17,15 +18,22 @@ module Sidekiq::Status
     # @param [String] queue the queue's name
     # @param [ConnectionPool] redis_pool optional redis connection pool
     def call(worker_class, msg, queue, redis_pool=nil)
-      job = Sidekiq::Job.new(msg, queue)
       initial_metadata = { 
         jid: msg['jid'],
         status: :queued,
         worker: worker_class, 
-        args: job.display_args.to_a.empty? ? nil : job.display_args.to_json
+        args: display_args(msg, queue)
       }
       store_for_id msg['jid'], initial_metadata, @expiration, redis_pool
       yield
+    end
+
+    def display_args(msg, queue)
+      job = Sidekiq::Job.new(msg, queue)
+      return job.display_args.to_a.empty? ? nil : job.display_args.to_json
+    rescue Exception => e
+      # For Sidekiq ~> 2.7
+      return msg['args'].to_a.empty? ? nil : msg['args'].to_json
     end
   end
 end
